febbb67a3c0da5d0afb83f32db2029ef79a25b7d
Matvey Eikholts <matze85eikholts@gmail.com>
2025-04-22 18:42:33 +0600

    empty spring framework app

---
+++ flowers-control/src/main/java/com/example/flowers_control/FlowersControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowers_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersControlApplication.class, args);
+	}
+
+}
